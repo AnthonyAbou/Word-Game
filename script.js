83aa343b3ee8b2d4cb6a3154581d2eb601bd9656
AnthonyAbou <aaaboujawdah2001@yahoo.com>
2022-02-27 12:00:15 -0800

    Implementing actual gameplay, modified board and script

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-var number = 1
+var number = 0
+var rowNumber = 1
+var answer = ["t","h","i","n","g"]
+var userAnswer = []
 
 document.addEventListener('keydown', function(e) {
-    
-        if(e.keyCode > 64 && e.keyCode < 91) {
-            document.getElementById('input'+number).value = e.key
-            number = number+1
-            console.log("+ current square " + number)
-        }
 
-        if(e.key.match('Backspace')) {
-            number = number-1
-            if(number == 0) {
-                number = 1
-            }
-            document.getElementById('input'+number).value = ''
-            console.log("- current square " + number)
+    if(e.keyCode > 64 && e.keyCode < 91) {
+        if(userAnswer.length == 5) {
+            return
         }
+        document.getElementById('row' + rowNumber).getElementsByClassName('userinput')[number].value = e.key
+        number = number + 1
+        userAnswer.push(e.key)
+        console.log("+ current square " + number)
+    }
 
+    if(e.key.match('Backspace')) {
+        number = number - 1
+        if(number == -1) {
+            number = 0
+        }
+        document.getElementById('row' + rowNumber).getElementsByClassName('userinput')[number].value = ''
+        userAnswer.splice(-1,1)
+        console.log("- current square " + number)
+    }
+
+    if(e.key.match('Enter')) {
+        console.log(userAnswer)
+        checkAns()
+    }
 });
 
+function checkAns() {
+    var square = document.getElementById('row' + rowNumber).querySelectorAll('.word-squares')
 
-
-/*
-function moveFocus(from, to) {
-    var length = from.value.length;
-    
-    document.addEventListener('keydown', function(e) {
-        if(e.code == 'Backspace') {
-            from.value = ''
-        }
-    });
-
-    if(length == 1) {
-        document.getElementById(to).focus();
+    if(userAnswer.length != 5) {
+        return
     }
-    
+
+    for(let i = 0; i < answer.length; i++) {
+        if(userAnswer[i] == answer[i]) {
+            square[i].style.backgroundColor = "green"
+        }
+        else if(answer.includes(userAnswer[i])) {
+            square[i].style.backgroundColor = "yellow"
+        }
+    }
+    userAnswer = []
+    number = 0
+    rowNumber = rowNumber + 1
 }
-*/
